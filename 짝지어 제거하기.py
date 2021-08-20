ef0f6c4059400258426228b291919457762cdd15
mango-jina <wlsdk194@gmail.com>
2021-08-20 13:21:53 +0900

    프로그래머스_짝지어 제거하기 커밋

---
+++ 짝지어 제거하기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    ls = [i for i in s]
+    stack = []
+    for l in ls:
+        if len(stack) == 0 or stack[-1] != l:
+            stack.append(l)
+        elif stack[-1] == l:
+            stack.pop()
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
+
+s = 'cdcd'
+print(solution(s))
