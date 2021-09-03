05c8d994423c94455574139a4540dcdc2aef4e12
mango-jina <wlsdk194@gmail.com>
2021-09-03 22:10:21 +0900

    프로그래머스_영어 끝말잇기 커밋

--- 영어 끝말잇기.py
+++ 영어 끝말잇기.py
@@ -4,17 +4,13 @@ def solution(n, words):
     for i, w in enumerate(words):
         if len(s)!= 0:
             if w[0] != s[-1][-1]:
-                break
+                return[(i%n)+1, ls[i%n]+1]
 
         if w not in s:
             s.append(w)
             ls[i%n] += 1
         else:
-            break
-
+            return[(i%n)+1, ls[i%n]+1]
+    
     if words == s:
         return [0,0]
-    else:
-        for i, l in enumerate(ls):
-            if l == min(ls):
-                return [i+1,l+1]
