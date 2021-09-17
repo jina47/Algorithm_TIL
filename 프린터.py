16b18f9558ca3a3458d46888d1a7d220571f21f3
mango-jina <wlsdk194@gmail.com>
2021-09-17 21:12:34 +0900

    프로그래머스_프린터 commit

--- 프린터.py
+++ 프린터.py
@@ -1,13 +1,23 @@
 def solution(priorities, location):
     answer = []
-    ip = [[i, p] for i, p in enumerate(priorities)] # [인덱스, 우선순위값] 리스트 생성
+
+    # [인덱스, 우선순위값] 리스트 생성
+    ip = [[i, p] for i, p in enumerate(priorities)]
     
-    while len(answer) != len(priorities): # answer 리스트에 우선순위에 따라 출력하는 순서대로 ip값 넣어줌
-        if ip[0][1] < max([i[1] for i in ip]): # 우선순위가 최대가 아니라면 후순위
-            ip.append(ip[0])
-            ip.pop(0)
-        else: # 우선순위가 최대값이라면 answer값에 ip값을 넣어주고 ip에서는 제거
-            answer.append(ip[0])
-            ip.pop(0)
-            if answer[-1][0] == location: # answer에 넣어준 값의 첫 번째 요소가 location 값과 같으면 len(answer)가 인쇄되는 차례이므로 return
+    # answer 리스트에 우선순위에 따라 출력하는 순서대로 인덱스값 넣어줌
+    while len(answer) != len(priorities):
+
+        # 판단해야 하는 요소를 factor로 지정
+        factor = ip.pop(0)
+
+        # 우선순위가 최대가 아니라면 ip에 맨 끝으로 추가
+        if ip and factor[1] < max([i[1] for i in ip]):
+            ip.append(factor)
+        
+        # 우선순위가 최대라면 answer값에 인덱스값을 추가
+        else: 
+            answer.append(factor[0])
+
+            # answer에 넣어준 값이 location 값과 같으면 len(answer)는 원하는 문서의 인쇄 차례이므로 return
+            if answer[-1] == location: 
                 return len(answer)
